8b07a0c2955606dacf52726fdd5821e08a610d20
VineJPS <vinicius108sousa@gmail.com>
2024-05-29 20:47:54 -0300

    HTML e CSS concluidos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const mario = document.querySelector(".mario")
+const jump = () =>{
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+        
+    }, 500)
+}
+
+document.addEventListener('keydown' , jump)
